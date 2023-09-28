1bee0efab5467e79ab3c0d23b398b9186562dd18
coderzbar <coderzbar@gmail.com>
2023-09-28 12:57:34 +0530

    working with auth

---
+++ src/lib/utils/UseToken.js
@@ -0,0 +1,21 @@
+import Cookies from "js-cookie";
+import { AUTH_TOKEN_KEY } from "lib/constants/utilsConstant";
+
+export function useToken() {
+  return {
+    setToken(token) {
+      Cookies.set(AUTH_TOKEN_KEY, token, { expires: 1 });
+    },
+    getToken() {
+      return Cookies.get(AUTH_TOKEN_KEY);
+    },
+    removeToken() {
+      Cookies.remove(AUTH_TOKEN_KEY);
+    },
+    hasToken() {
+      const token = Cookies.get(AUTH_TOKEN_KEY);
+      if (!token) return false;
+      return true;
+    },
+  };
+}
--- src/pages/onboarding/index.jsx
+++ src/pages/onboarding/index.jsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { FormInput } from "components/Form-Input/Input";
+import { onboardSchema } from "app/common/auth/validation";
 
 import "styles/OnBoarding.css";
-
 import logo from "app/assets/logo.png";
-
 import user from "app/assets/user-img.jpg";
-
-import { FileUploader } from "react-drag-drop-files";
+import { useToken } from "lib/utils/UseToken";
+import { useNavigate } from "react-router-dom";
 
 export const Onboarding = () => {
+  const navigate = useNavigate();
   const fileTypes = ["JPG", "PNG", "GIF"];
   const [file, setFile] = useState(null);
   const [img, setImg] = useState(user);
+
+  const { setToken } = useToken();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(onboardSchema) });
   const handleChange = (file) => {
     setFile(file);
   };
@@ -22,6 +35,15 @@ export const Onboarding = () => {
       setImg(objectUrl);
     }
   }, [file]);
+
+  const onBoard = (data) => {
+    localStorage.setItem(
+      "auth",
+      JSON.stringify({ userData: data, auth: true })
+    );
+    setToken(true);
+    navigate("/");
+  };
 
   return (
     <div className="onboarding_page">
@@ -42,29 +64,40 @@ export const Onboarding = () => {
           <h2>Setup your account</h2>
           <div className="setup_select">
             <p className="setup-sub-head">What best describes you?</p>
-            <select class="form-select">
-              <option selected>Creator/Influencer/Model</option>
-              <option value="1">Creator</option>
-              <option value="2">Influencer</option>
-              <option value="3">Model</option>
+            <select className="form-select" {...register("select")}>
+              <option value="">Creator/Influencer/Model</option>
+              <option value="creator">Creator</option>
+              <option value="influencer">Influencer</option>
+              <option value="model">Model</option>
             </select>
+            {errors?.select && (
+              <p className="errorMessage">{errors?.select?.message}</p>
+            )}
           </div>
           <div className="profile_set">
             <p className="setup-sub-head">Enter Your profile details</p>
-            <form>
+            <form onSubmit={handleSubmit(onBoard)}>
               <div className="mb-3">
-                <input
+                <FormInput
                   type="text"
                   className="form-control input_onb"
                   placeholder="Vivian Stuart"
+                  {...register("profileName")}
                 />
+                {errors?.profileName && (
+                  <p className="errorMessage">{errors?.profileName?.message}</p>
+                )}
               </div>
               <div className="mb-3">
-                <input
+                <FormInput
                   type="text"
                   className="form-control input_onb"
                   placeholder="@vivstuart"
+                  {...register("userName")}
                 />
+                {errors?.userName && (
+                  <p className="errorMessage">{errors?.userName?.message}</p>
+                )}
               </div>
               <div className="upload_profile">
                 <p className="setup-sub-head">Upload a picture</p>
@@ -76,6 +109,7 @@ export const Onboarding = () => {
                   name="file"
                   types={fileTypes}
                   multiple={false}
+                  maxSize={1}
                   hoverTitle="Drop here"
                 />
               </div>
